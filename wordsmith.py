e4764ac9b3b2647a1d4899976ae8e0478296b3ad
Daniel Dugovic <dandydand@gmail.com>
2021-02-11 21:36:06 -0600

    Fix #7

--- wordsmith.py
+++ wordsmith.py
@@ -8,137 +8,130 @@ import random as rd
 config = cf.config()
 dictionary.open_files()
 initc = config.channels.keys()
-bot = commands.Bot(
-    # set up the bot
-    irc_token=config.irc_token,
-    client_id=config.client_id,
-    nick=config.nick,
-    prefix='!',
-    initial_channels=initc
-)
 custom_commands = cf.custom_commands()
 
-@bot.event
-async def event_ready():
-    'Called once when the bot goes online.'
-    print('Wordsmith 0.1 by Danielle Barker')
+class Bot(commands.Bot):
 
-@bot.event
-async def event_message(ctx):
-    if ctx.content in custom_commands.keys():
-        f = open(custom_commands[ctx.content], 'r')
-        messages = f.read().split('\n')
-        message = rd.choice(messages)
-        print("message",message)
-        await ctx.channel.send(rd.choice(messages))
-        
+    def __init__(self):
+        super().__init__(irc_token=config.irc_token, client_id=config.client_id, nick=config.nick, prefix='!',
+                         initial_channels=initc)
 
-    else:
-        await bot.handle_commands(ctx)
-    
+    async def event_ready(self):
+        print(f'Wordsmith 0.1 by Danielle Barker | {self.nick}')
 
-@bot.command(name='define')
-async def define(ctx,word):
-    msg = dictionary.define(word.upper(),config.channels[ctx.channel.name]["lexicon"])
-    await ctx.send(msg)
+    async def event_message(self, ctx):
+        print(ctx.content)
+        if ctx.content in custom_commands.keys():
+            f = open(custom_commands[ctx.content], 'r')
+            messages = f.read().split('\n')
+            message = rd.choice(messages)
+            print("message",message)
+            await ctx.channel.send(rd.choice(messages))
+        else:
+            await self.handle_commands(ctx)
 
-@bot.command(name='lexicon')
-async def lexicon(ctx,word):
-    if ctx.author.name == ctx.channel.name or ctx.author.is_mod:
-        config.channels[ctx.channel.name]["lexicon"]=word.lower()
-        cf.save(config)
-        msg = f'Lexicon changed to {word.lower()}'
-    else:
-        msg = f'Command can only be used by {ctx.channel.name} or moderators'
-    await ctx.send(msg)
+    @commands.command(name='define')
+    async def define(self, ctx, word):
+        msg = dictionary.define(word.upper(),config.channels[ctx.channel.name]["lexicon"])
+        await ctx.send(msg)
 
-@bot.command(name='so')
-async def shoutout(ctx,word):
-    if ctx.author.name == ctx.channel.name or ctx.author.is_mod:
-        msg = f'Welcome http://twitch.tv/{word.lower()} !'
-    else:
-        msg = f'Command can only be used by {ctx.channel.name} or moderators'
-    await ctx.send(msg)
+    @commands.command(name='lexicon')
+    async def lexicon(self, ctx, word):
+        if ctx.author.name == ctx.channel.name or ctx.author.is_mod:
+            config.channels[ctx.channel.name]["lexicon"]=word.lower()
+            cf.save(config)
+            msg = f'Lexicon changed to {word.lower()}'
+        else:
+            msg = f'Command can only be used by {ctx.channel.name} or moderators'
+        await ctx.send(msg)
 
-@bot.command(name='related')
-async def related(ctx,word):
-    msg = dictionary.related(word.upper(),config.channels[ctx.channel.name]["lexicon"])
-    num = msg[0]
-    msg = msg[1]
-    print(len(msg))
-    await ctx.send(f'{num} results found:\n{msg}')
+    @commands.command(name='so')
+    async def shoutout(self, ctx, word):
+        if ctx.author.name == ctx.channel.name or ctx.author.is_mod:
+            msg = f'Welcome http://twitch.tv/{word.lower()} !'
+        else:
+            msg = f'Command can only be used by {ctx.channel.name} or moderators'
+        await ctx.send(msg)
 
-@bot.command(name='startswith')
-async def startswith(ctx,word):
-    msg = dictionary.starts_with(word.upper(),config.channels[ctx.channel.name]["lexicon"])
-    num = msg[0]
-    msg = msg[1]
-    print(len(msg))
-    await ctx.send(f'{num} results found:\n{msg}')
+    @commands.command(name='related')
+    async def related(self, ctx, word):
+        msg = dictionary.related(word.upper(),config.channels[ctx.channel.name]["lexicon"])
+        num = msg[0]
+        msg = msg[1]
+        print(len(msg))
+        await ctx.send(f'{num} results found:\n{msg}')
 
-@bot.command(name='endswith')
-async def endswith(ctx,word):
-    msg = dictionary.ends_with(word.upper(),config.channels[ctx.channel.name]["lexicon"])
-    num = msg[0]
-    msg = msg[1]
-    print(len(msg))
-    await ctx.send(f'{num} results found:\n{msg}')
+    @commands.command(name='startswith')
+    async def startswith(self, ctx, word):
+        msg = dictionary.starts_with(word.upper(),config.channels[ctx.channel.name]["lexicon"])
+        num = msg[0]
+        msg = msg[1]
+        print(len(msg))
+        await ctx.send(f'{num} results found:\n{msg}')
 
-@bot.command(name='contains')
-async def contains(ctx,word):
-    msg = dictionary.contains(word.upper(),config.channels[ctx.channel.name]["lexicon"])
-    num = msg[0]
-    msg = msg[1]
-    print(len(msg))
-    await ctx.send(f'{num} results found:\n{msg}')
+    @commands.command(name='endswith')
+    async def endswith(self, ctx, word):
+        msg = dictionary.ends_with(word.upper(),config.channels[ctx.channel.name]["lexicon"])
+        num = msg[0]
+        msg = msg[1]
+        print(len(msg))
+        await ctx.send(f'{num} results found:\n{msg}')
 
-@bot.command(name='pattern')
-async def pattern(ctx,word):
-    msg = dictionary.pattern(word.upper(),config.channels[ctx.channel.name]["lexicon"])
-    num = msg[0]
-    msg = msg[1]
-    print(len(msg))
-    await ctx.send(f'{num} results found:\n{msg}')
+    @commands.command(name='contains')
+    async def contains(self, ctx, word):
+        msg = dictionary.contains(word.upper(),config.channels[ctx.channel.name]["lexicon"])
+        num = msg[0]
+        msg = msg[1]
+        print(len(msg))
+        await ctx.send(f'{num} results found:\n{msg}')
 
-@bot.command(name='regex')
-async def regex(ctx,word):
-    msg = dictionary.regex(word.upper(),config.channels[ctx.channel.name]["lexicon"])
-    num = msg[0]
-    msg = msg[1]
-    print(len(msg))
-    await ctx.send(f'{num} results found:\n{msg}')
+    @commands.command(name='pattern')
+    async def pattern(self, ctx, word):
+        msg = dictionary.pattern(word.upper(),config.channels[ctx.channel.name]["lexicon"])
+        num = msg[0]
+        msg = msg[1]
+        print(len(msg))
+        await ctx.send(f'{num} results found:\n{msg}')
 
-@bot.command(name='info')
-async def info(ctx,word):
-    msg = dictionary.info(word.upper(),config.channels[ctx.channel.name]["lexicon"])
-    await ctx.send(msg)
+    @commands.command(name='regex')
+    async def regex(self, ctx, word):
+        msg = dictionary.regex(word.upper(),config.channels[ctx.channel.name]["lexicon"])
+        num = msg[0]
+        msg = msg[1]
+        print(len(msg))
+        await ctx.send(f'{num} results found:\n{msg}')
 
+    @commands.command(name='info')
+    async def info(self, ctx, word):
+        msg = dictionary.info(word.upper(),config.channels[ctx.channel.name]["lexicon"])
+        await ctx.send(msg)
 
-@bot.command(name='anagram')
-async def anagram(ctx,word):
-    msg = dictionary.anagram_1(word.upper(),config.channels[ctx.channel.name]["lexicon"])
-    num = msg[0]
-    msg = msg[1]
-    print(len(msg))
-    await ctx.send(f'{num} results found:\n{msg}')
+    @commands.command(name='anagram')
+    async def anagram(self, ctx, word):
+        msg = dictionary.anagram_1(word.upper(),config.channels[ctx.channel.name]["lexicon"])
+        num = msg[0]
+        msg = msg[1]
+        print(len(msg))
+        await ctx.send(f'{num} results found:\n{msg}')
 
+    @commands.command(name='random')
+    async def random(ctx):
+        msg = dictionary.random_word(config.channels[ctx.channel.name]["lexicon"])
+        await ctx.send(msg)
 
-@bot.command(name='random')
-async def random(ctx):
-    msg = dictionary.random_word(config.channels[ctx.channel.name]["lexicon"])
-    await ctx.send(msg)
+    @commands.command(name='pronounce')
+    async def pronounce(ctx, word):
+        if word.upper() in dictionary.wordlist[config.channels[ctx.channel.name]["lexicon"]]:
+            await ctx.send(f'https://www.collinsdictionary.com/sounds/hwd_sounds/en_gb_{word.lower()}.mp3')
+        else:
+            await ctx.send(f'{word} is not a valid word')
 
-@bot.command(name='pronounce')
-async def pronounce(ctx, word):
-    if word.upper() in dictionary.wordlist[config.channels[ctx.channel.name]["lexicon"]]:
-        await ctx.send(f'https://www.collinsdictionary.com/sounds/hwd_sounds/en_gb_{word.lower()}.mp3')
-    else:
-        await ctx.send(f'{word} is not a valid word')
-@bot.command(name='crypto')
-async def crypto(ctx, word):
-    msg = dictionary.crypto(word.upper(),config.channels[ctx.channel.name]["lexicon"])
-    num = msg[0]
-    msg = msg[1]
-    await ctx.send(f'{num} results found:\n{msg}')
+    @commands.command(name='crypto')
+    async def crypto(ctx, word):
+        msg = dictionary.crypto(word.upper(),config.channels[ctx.channel.name]["lexicon"])
+        num = msg[0]
+        msg = msg[1]
+        await ctx.send(f'{num} results found:\n{msg}')
 
+bot = Bot()
 bot.run()
