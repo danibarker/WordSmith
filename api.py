c13124025f4a03947b289b1a63383e7471c5b834
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-08-01 13:09:20 -0500

    Attempt to fix prediction command

--- api.py
+++ api.py
@@ -2,9 +2,9 @@ import requests
 
 
 def common(word):
-    context = { 'accept': 'application/json', 'authority': 'woogles.io', 'origin': 'https://woogles.io', 'User-Agent': 'wordsmith-bot' }
+    authorization = { 'authority': 'woogles.io', 'origin': 'https://woogles.io', 'User-Agent': 'wordsmith-bot' }
     request = { 'lexicon': 'ECWL', 'words': [word], 'definitions': False }
-    response = requests.post('https://woogles.io/twirp/word_service.WordService/DefineWords', json=request, headers=context)
+    response = requests.post('https://woogles.io/twirp/word_service.WordService/DefineWords', json=request, headers=authorization)
     values = response.json()
     try:
         return values['results'][word]['v']
@@ -13,8 +13,9 @@ def common(word):
 
 
 def equity(rack, lexicon):
-    parameters = {'rack': rack, 'lexicon': lexicon}
-    response = requests.get('https://cross-tables.com/leaves_values.php', headers={'User-Agent': 'wordsmith-bot'}, params=parameters)
+    authorization = { 'User-Agent': 'wordsmith-bot' }
+    parameters = { 'rack': rack, 'lexicon': lexicon }
+    response = requests.get('https://cross-tables.com/leaves_values.php', headers=authorization, params=parameters)
     values = response.json()
     try:
         return (values['rack'], values['rack-value'])
@@ -23,19 +24,18 @@ def equity(rack, lexicon):
 
 
 def predict(config, name, player, opponent):
-    authorization = {'Authorization': 'Bearer ' + config.api_token, 'Client-Id': config.client_id}
-    parameters = {'login': name}
+    authorization = { 'Authorization': 'Bearer ' + config.api_token, 'Client-Id': config.client_id }
+    parameters = { 'login': name }
     response = requests.get('https://api.twitch.tv/helix/users', headers=authorization, params=parameters)
     values = response.json()
     try:
         broadcasterID = values['id']
-        player = values['display_name']
     except KeyError:
         return str(values['status']) + ' ' + values['message']
 
-    outcomes = [{'title': player}, {'title': opponent}]
-    parameters = {'broadcaster_id': broadcasterID, 'title': 'Who will win?', 'outcomes': outcomes, 'prediction_window': 300}
-    response = requests.get('https://api.twitch.tv/helix/predictions', headers=authorization, params=parameters)
+    outcomes = [{ 'title': player }, { 'title': opponent }]
+    request = { 'broadcaster_id': broadcasterID, 'title': 'Who will win?', 'outcomes': outcomes, 'prediction_window': 300 }
+    response = requests.post('https://api.twitch.tv/helix/predictions', json=request, headers=authorization)
     values = response.json()
     try:
         return values['title'] + ': ' + str(values['status'])
